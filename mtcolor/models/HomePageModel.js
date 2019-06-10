f47008edb58d57a65d2ea0f9f1f94cdd5f95dcdb
tariq <tmakled@gmail.com>
2019-06-10 09:44:50 -0400

    remove old model

--- mtcolor/models/HomePageModel.js
+++
@@ -1,7 +0,0 @@
-module.exports = class HomePageModel {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-};
-//# sourceMappingURL=HomePageModel.js.map
